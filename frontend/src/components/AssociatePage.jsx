56f2c5e304d6f6c8a1d5212531b66ab3363c26c7
Daksh <dyashlaha039909@gmail.com>
2020-05-17 17:35:38 +0530

    Frontend: Add basic routing for Home, Customer, Associate and Error Pages

---
+++ frontend/src/components/AssociatePage.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class AssociatePage extends Component{
+    constructor(props) {
+        super(props);
+        this.dtate = {};
+    }
+
+    render() {
+        return (
+            <p>AssociatePage</p>
+        )
+    }
+}
+
+export default AssociatePage;
---
+++ frontend/src/components/ErrorPage.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class ErrorPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <p>ErrorPage</p>
+        )
+    }
+}
+
+export default ErrorPage;
