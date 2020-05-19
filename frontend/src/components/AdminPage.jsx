dd50d07c6a53b8c2d47616af1cc292b23723dc7c
Daksh <dyashlaha039909@gmail.com>
2020-05-19 06:26:33 +0530

    Frontend: Add basic AdminPage and AssociatePage Layout

--- frontend/src/components/AdminPage.jsx
+++ frontend/src/components/AdminPage.jsx
@@ -1,14 +1,76 @@
 import React, {Component} from "react";
+import {Container,
+        Navbar,
+        Nav,
+        Card,
+        Button,
+        Row,
+        Col,
+        Accordion,
+    } from "react-bootstrap";
 
 class AdminPage extends Component{
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {
+            customer: true,
+            associate: false,
+        };
+    }
+
+    handleCustomer = () => {
+        this.setState({customer: true, associate: false});
+    }
+
+    handleAssociate = () => {
+        this.setState({customer: false, associate: true});
+    }
+
+    customerLayout = () => {
+        return (
+            <Card>
+                <Card.Body>
+                    <Card.Title>
+                        <Row>
+                            <Col md={12}>
+                                List of Customers
+                            </Col>
+                        </Row>
+                    </Card.Title>
+                </Card.Body>
+            </Card>
+        )
+    }
+
+    associateLayout = () => {
+        return (
+            <Card>
+                <Card.Body>
+                    <Card.Title>
+                        <Row>
+                            <Col md={12}>
+                                List of Associates
+                            </Col>
+                        </Row>
+                    </Card.Title>
+                </Card.Body>
+            </Card>
+        )
     }
 
     render() {
         return (
-            <p>AdminPage</p>
+            <Container>
+                <Navbar bg="primary" variant="dark">
+                    <Navbar.Brand href="/home">Walmart</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#customer" onClick={this.handleCustomer}>Customers</Nav.Link>
+                        <Nav.Link href="#associate" onClick={this.handleAssociate}>Associates</Nav.Link>
+                    </Nav>
+                </Navbar>
+                {this.state.customer && this.customerLayout()}
+                {this.state.associate && this.associateLayout()}
+            </Container>
         )
     }
 }
